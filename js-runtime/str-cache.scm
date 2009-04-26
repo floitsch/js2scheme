5d7dd15e366fb03369e9335df8af250e2b4d99b7
Florian Loitsch <Florian Loitsch@localhost>
2009-04-26 21:03:12 +0200

    extracts STR literals. Not used yet, though.

---
+++ js-runtime/str-cache.scm
@@ -0,0 +1,123 @@
+(module str-cache
+   (main my-main))
+
+(define *seen-str-include?* #f)
+(define *strs* '())
+
+(define *in-file* #f)
+(define *out-file* #f)
+
+(define (my-main args)
+   (args-parse (cdr args)
+      (("-o" ?file (help "Out-File file"))
+       (set! *out-file* file))
+      (else
+       (when *in-file*
+	  (error "str-cache"
+		 "Only one in-file allowed"
+		 else)
+	  (args-parse-usage #f))
+       (set! *in-file* else)))
+   (when (or (not *in-file*)
+	     (not *out-file*))
+      (error "str-cache"
+	     "in-file or out-file missing"
+	     #f))
+   (display (format "writing STR-caches for ~a\n" *in-file*) (current-error-port))
+
+   (extract-and-verify-STRs!)
+   (write-STR-cache!))
+
+
+(define (write-STR-cache!)
+   (let ((out-p (if (string=? *out-file* "-")
+		    (current-output-port)
+		    (open-output-file *out-file*))))
+      (with-handler
+	 (lambda (e)
+	    (when (not (string=? *out-file* "-"))
+	       (delete-file *out-file*))
+	    (raise e))
+	 (unwind-protect
+	    (let ((cache-ids (map (lambda (str)
+				     (gensym 'str))
+				  *strs*)))
+	       (display ";; GENERATED FILE -- DO NOT EDIT\n" out-p)
+	       (display ";; GENERATED FILE -- DO NOT EDIT\n" out-p)
+	       (display ";; GENERATED FILE -- DO NOT EDIT\n" out-p)
+	       (display ";; GENERATED FILE -- DO NOT EDIT\n" out-p)
+	       (display ";; GENERATED FILE -- DO NOT EDIT\n" out-p)
+	       (unless (null? *strs*)
+		  (pp `(define-macro (STR str)
+			  (cdr (assoc str ',(map cons *strs* cache-ids))))
+		      out-p)
+		  (for-each (lambda (id str)
+			       (pp `(define ,id (utf8->js-string-literal ,str))
+				   out-p))
+			    cache-ids
+			    *strs*)))
+	    (close-output-port out-p)))))
+
+(define (extract-and-verify-STRs!)
+   (let ((in-p (if (string=? *in-file* "-")
+		   (current-input-port)
+		   (open-input-file *in-file*))))
+      (unwind-protect
+	 (extract in-p)
+	 (when (and (pair? *strs*)
+		    (not *seen-str-include?*))
+	    (warning "str-cache" "could not find str-include" *in-file*))
+	 (when (and (null? *strs*)
+		    *seen-str-include?*)
+	    (warning "str-cache" "str-include without STRs" *in-file*))
+	 (close-input-port in-p))))
+
+(define (extract in-p)
+   (let ((e (read in-p)))
+      (unless (eof-object? e)
+	 (extract-expr! e)
+	 (extract in-p))))
+
+(define (add-str! str)
+   (unless (member str *strs*)
+      (set! *strs* (cons str *strs*))))
+
+;; we do not handle quotes, etc. Should not be necessary for now.
+(define (extract-expr! e)
+   (match-case e
+      ((define (STR . ?-) . ?-) 'do-nothing)
+      ((define-macro (STR . ?-) . ?-) 'do-nothing)
+      ((define-inline (STR . ?-) . ?-) 'do-nothing)
+      ((define-macro (define-runtime-globals . ?-) . ?-) 'do-nothing)
+      ((define-runtime-globals . ?defs)
+       (for-each (lambda (def)
+		    (match-case def
+		       ((define (?id . ?-) . ?body)
+			(add-str! (symbol->string id))
+			(for-each extract-expr! body))
+		       (else
+			(extract-expr! def))))
+		 defs))
+      ((STR (and ?str (? string?)))
+       (add-str! str))
+      ((STR . ?-)
+       (warning "str-cache" "bad STR form" e))
+      ((include ?f)
+       (when (and *seen-str-include?*
+		  (string=? (prefix f) "strs"))
+	  (warning "str-cache" "already seen string-include" e))
+       ;; for now just see if they are equal. we can implement more
+       ;; sophisticated techniques later.
+       (when (not (string=? f *out-file*))
+	  (warning "str-cache" "string-include and out-file don't match"
+		   f
+		   *out-file*))
+       (set! *seen-str-include?* #t))
+      ((?- . ?-)
+       (let loop ((e e))
+	  (cond
+	     ((null? e) 'done)
+	     ((pair? e) (extract-expr! (car e))
+			(loop (cdr e)))
+	     (else (extract-expr! e)))))
+      (else 'do-nothing)))
